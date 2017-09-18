f271a341c45c57d62ff65322be7ff61a2dde8eb3
tao.j <tao.j@tcl.com>
2017-09-18 18:51:14 +0800

    get singer list

--- pattern_test.py
+++ pattern_test.py
@@ -1,20 +1,59 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import requests
+from bs4 import BeautifulSoup
+
 import threading
 from time import sleep
 
 
-def run(name):
-    sleep(3)
-    print name, ' run'
+# def run(name):
+#     sleep(3)
+#     print name, ' run'
+#
+# threads = []
+# for i in range(10):
+#     t = threading.Thread(target=run, kwargs={'name':"t"+str(i)})
+#     t.setDaemon(True)
+#     t.start()
+#     threads.append(t)
+#
+# for t in threads:
+#     t.join()
 
-threads = []
-for i in range(10):
-    t = threading.Thread(target=run, kwargs={'name':"t"+str(i)})
-    t.setDaemon(True)
-    t.start()
-    threads.append(t)
+def get_top_100_female_singers():
+    # response = requests.get('http://music.baidu.com/artist/cn/male')
+    response = requests.get('https://music.163.com/discover/artist/cat?id=1002')
+    soup = BeautifulSoup(response.text, 'lxml')
+    # < ul class ="m-cvrlst m-cvrlst-5 f-cb" id="m-artist-box" >
+    singers = soup.find('ul', attrs={'class':'m-cvrlst m-cvrlst-5 f-cb'}).find_all('a', attrs={'class':'nm nm-icn f-thide s-fc0'})
+    print len(singers)
+    for singer in singers:
+        print singer
+    # print response.content
 
-for t in threads:
-    t.join()
+def get_top_100_male_singers():
+    # response = requests.get('http://music.baidu.com/artist/cn/male')
+    response = requests.get('https://music.163.com/discover/artist/cat?id=1001')
+    soup = BeautifulSoup(response.text, 'lxml')
+    # < ul class ="m-cvrlst m-cvrlst-5 f-cb" id="m-artist-box" >
+    singers = soup.find('ul', attrs={'class':'m-cvrlst m-cvrlst-5 f-cb'}).find_all('a', attrs={'class':'nm nm-icn f-thide s-fc0'})
+    print len(singers)
+    for singer in singers:
+        print singer
+
+def get_top_100_band():
+    # response = requests.get('http://music.baidu.com/artist/cn/male')
+    response = requests.get('https://music.163.com/discover/artist/cat?id=1003')
+    soup = BeautifulSoup(response.text, 'lxml')
+    # < ul class ="m-cvrlst m-cvrlst-5 f-cb" id="m-artist-box" >
+    singers = soup.find('ul', attrs={'class':'m-cvrlst m-cvrlst-5 f-cb'}).find_all('a', attrs={'class':'nm nm-icn f-thide s-fc0'})
+    print len(singers)
+    for singer in singers:
+        print singer
+
+get_top_100_male_singers()
+get_top_100_female_singers()
+get_top_100_band()
+
